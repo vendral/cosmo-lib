a75275eca1f263fc3be42ed4799d9ea6d1624036
vendral <marek.krupa.mk@gmail.com>
2020-10-05 20:44:11 +0200

    [Backend] Add simple http example

--- bnd/main.go
+++ bnd/main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
+	"net/http"
 )
 
+var (
+	ServerPort = 8090
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(w, "hello\n")
+}
+
 func main() {
-    fmt.Println("Hello Cosmo!")
+
+	StringServerPort := fmt.Sprintf("%s%v", ":", ServerPort)
+
+	http.HandleFunc("/cosmo", hello)
+
+	fmt.Println("Cosmo-lib is listening on port:", ServerPort)
+	fmt.Printf("Go to Cosmo-lib: http://localhost%s", StringServerPort)
+
+	http.ListenAndServe(StringServerPort, nil)
 }
