8141ab173762b1bfaf917169d8b282c0383dc924
vendral <marek.krupa.mk@gmail.com>
2020-11-06 19:17:32 +0100

    [Backend] Add simple handlers and templates

--- bnd/main.go
+++ bnd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
@@ -9,18 +10,36 @@ var (
 	ServerPort = 8090
 )
 
-func hello(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprint(w, "hello\n")
+var tmpl = template.Must(template.ParseFiles("tmpl/index.html"))
+
+func indexHandler(w http.ResponseWriter, req *http.Request) {
+	tmpl.Execute(w, nil)
+}
+
+func findHandler(w http.ResponseWriter, req *http.Request) {
+	if req.Method == "POST" {
+		req.ParseForm()
+
+		fmt.Println()
+		fmt.Println("Cosmo-lib is trying to find a book!")
+		fmt.Printf("Author: %s, Title: %s", req.Form["author"], req.Form["title"])
+	}
 }
 
 func main() {
 
 	StringServerPort := fmt.Sprintf("%s%v", ":", ServerPort)
 
-	http.HandleFunc("/cosmo", hello)
+	mux := http.NewServeMux()
+	fs := http.FileServer(http.Dir("assets"))
+
+	mux.Handle("/assets/", http.StripPrefix("/assets/", fs))
+
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/find", findHandler)
 
 	fmt.Println("Cosmo-lib is listening on port:", ServerPort)
 	fmt.Printf("Go to Cosmo-lib: http://localhost%s", StringServerPort)
 
-	http.ListenAndServe(StringServerPort, nil)
+	http.ListenAndServe(StringServerPort, mux)
 }
